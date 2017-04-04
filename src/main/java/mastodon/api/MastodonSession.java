dcec3e5b121ec697dbae13a8cb961e67ff4b9f8d
zyuiop <zyuiop@zyuiop.net>
2017-04-04 03:38:50 +0200

    Improve Readme and add Raw class

--- src/main/java/mastodon/api/MastodonSession.java
+++ src/main/java/mastodon/api/MastodonSession.java
@@ -1,14 +1,17 @@
 package mastodon.api;
 
 import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.http.HttpContent;
 import com.google.api.client.http.HttpRequest;
 import com.google.api.client.http.HttpResponse;
+import com.google.api.client.json.GenericJson;
 import com.google.gson.reflect.TypeToken;
 import mastodon.api.internal.FormDataContent;
 import mastodon.api.objects.MastodonAccount;
 import mastodon.api.objects.MastodonStatus;
 
 import java.io.IOException;
+import java.lang.reflect.Type;
 import java.util.List;
 
 import static mastodon.api.internal.RequestsHelper.factory;
@@ -20,45 +23,33 @@ import static mastodon.api.internal.RequestsHelper.getUrl;
 public class MastodonSession {
     private final MastodonApp app;
     private final Credential credential;
+    private final Raw raw = new Raw();
 
     MastodonSession(MastodonApp app, Credential credential) {
         this.app = app;
         this.credential = credential;
     }
 
-    private HttpResponse getRequest(String endpoint) throws IOException {
-        HttpRequest request = factory().buildGetRequest(getUrl(app.getMastodonInstance(), endpoint));
-        credential.initialize(request);
-        return request.execute();
-    }
-
-
-    private HttpResponse postRequest(String endpoint, FormDataContent content) throws IOException {
-        HttpRequest request = factory().buildPostRequest(getUrl(app.getMastodonInstance(), endpoint), content);
-        credential.initialize(request);
-        return request.execute();
-    }
-
     public MastodonAccount getAccount(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id).parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id, MastodonAccount.class);
     }
 
     public MastodonAccount getCurrentUser() throws IOException {
-        return getRequest("/api/v1/accounts/verify_credentials").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/verify_credentials", MastodonAccount.class);
     }
 
     public List<MastodonAccount> getFollowers(int id) throws IOException {
         TypeToken<List<MastodonAccount>> list = new TypeToken<List<MastodonAccount>>() {
         };
 
-        return (List<MastodonAccount>) getRequest("/api/v1/accounts/" + id + "/followers").parseAs(list.getType());
+        return raw.get("/api/v1/accounts/" + id + "/followers", list.getType());
     }
 
     public List<MastodonAccount> getFollowing(int id) throws IOException {
         TypeToken<List<MastodonAccount>> list = new TypeToken<List<MastodonAccount>>() {
         };
 
-        return (List<MastodonAccount>) getRequest("/api/v1/accounts/" + id + "/following").parseAs(list.getType());
+        return raw.get("/api/v1/accounts/" + id + "/following", list.getType());
     }
 
     public List<MastodonStatus> getStatuses(int id) throws IOException {
@@ -70,33 +61,67 @@ public class MastodonSession {
         TypeToken<List<MastodonStatus>> list = new TypeToken<List<MastodonStatus>>() {
         };
 
-        return (List<MastodonStatus>) getRequest("/api/v1/accounts/" + id + "/statuses" + param).parseAs(list.getType());
+        return raw.get("/api/v1/accounts/" + id + "/statuses" + param, list.getType());
     }
 
     public MastodonAccount follow(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/follow").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/follow", MastodonAccount.class);
     }
 
     public MastodonAccount unfollow(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/unfollow").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/unfollow", MastodonAccount.class);
     }
 
     public MastodonAccount block(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/block").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/block", MastodonAccount.class);
     }
 
     public MastodonAccount unblock(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/unblock").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/unblock", MastodonAccount.class);
     }
 
     public MastodonAccount mute(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/mute").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/mute", MastodonAccount.class);
     }
 
     public MastodonAccount unmute(int id) throws IOException {
-        return getRequest("/api/v1/accounts/" + id + "/unmute").parseAs(MastodonAccount.class);
+        return raw.get("/api/v1/accounts/" + id + "/unmute", MastodonAccount.class);
+    }
+
+    public Raw raw() {
+        return raw;
     }
 
     // Todo : relationships
     // Todo : all the rest :D
+
+    public class Raw {
+        public <T extends GenericJson> T get(String endpoint, Class<T> tClass) throws IOException {
+            return getUnparsed(endpoint).parseAs(tClass);
+        }
+
+        public <T extends GenericJson> T get(String endpoint, Type tClass) throws IOException {
+            return (T) getUnparsed(endpoint).parseAs(tClass);
+        }
+
+        public HttpResponse getUnparsed(String endpoint) throws IOException {
+            HttpRequest request = factory().buildGetRequest(getUrl(app.getMastodonInstance(), endpoint));
+            credential.initialize(request);
+            return request.execute();
+        }
+
+        public <T extends GenericJson> T post(String endpoint, HttpContent content, Class<T> tClass) throws IOException {
+            return postUnparsed(endpoint, content).parseAs(tClass);
+        }
+
+        public <T extends GenericJson> T post(String endpoint, HttpContent content, Type tClass) throws IOException {
+            return (T) postUnparsed(endpoint, content).parseAs(tClass);
+        }
+
+        public HttpResponse postUnparsed(String endpoint, HttpContent content) throws IOException {
+            HttpRequest request = factory().buildPostRequest(getUrl(app.getMastodonInstance(), endpoint), content);
+            credential.initialize(request);
+            return request.execute();
+        }
+    }
 }
